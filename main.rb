e87d6d8dac118e25c27483a5e89105ac39a4f885
wRadion <wradion@gmail.com>
2018-08-05 14:54:24 +0200

    Discord bot initialization

--- main.rb
+++ main.rb
@@ -8,18 +8,19 @@ Bundler.require(:default)
 
 # Database
 require_relative 'data/db.rb'
-
 # Config
-require_relative 'config/startup.rb'
-
+require_relative 'config/discord_bot.rb'
 # Extensions
 require_relative 'ext/include'
-
 # Lib
 require_relative 'lib/include'
-
 # Models
 require_relative 'models/include'
-
 # Commands
 require_relative 'commands/include'
+
+bot = Discordrb::Commands::CommandBot.new(DISCORD_CONFIG)
+::Commands.register_in(bot)
+bot.run
+
+exit 0
